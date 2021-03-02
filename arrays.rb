3ea65fcda498ca9c3eddb0d5de232249ffb31b42
Phyllis Njoroge <phyllisgetcity@gmail.com>
2021-03-02 16:24:08 +0000

    Automatically backed up by Learn

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+famous_cats = ["lil' bub", "grumpy cat", "maru"]
+p famous_cats.sort
+p famous_cats
+p famous_cats.sort!
+p famous_cats
---
+++ example_file.rb
@@ -0,0 +1,22 @@
+def a_method(a,b)
+  puts "I got #{a}"
+  puts "I got #{b}"
+  sum = a + b
+  puts "I got #{sum}"
+  sum
+end
+ 
+p a_method(2,3) 
+p "Hello World"
+
+p 6+3
+p instructor= "Tim"
+p total = 5 +  4
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+p stylish_chef
